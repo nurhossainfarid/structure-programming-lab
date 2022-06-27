a59ea9398c256534ba209e00249b3fbcf6609ead
nurhossainfarid <faahsan1516@gmail.com>
2022-06-27 21:12:10 +0600

    add fibonacci array

--- PracticeForFinal/Array/Fibonacci/Fibonacci.c
+++ PracticeForFinal/Array/Fibonacci/Fibonacci.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
 int main() {
-    int i, n, first = 0, second = 1;
+    int fibonacci[100], i, n;
+
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+
+    printf("Enter your fibonacci number : ");
+    scanf("%d", &n);
+
+    for ( i = 2; i < n; i++)
+    {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d", fibonacci[i]);
+    }
+    
 }
